93e6590896248c292a766721ea236768b1697ccc
Juan Pablo <juanpablovalles@gmail.com>
2024-07-11 19:28:52 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,16 +2,12 @@ import { createPool } from 'mysql2/promise';
 
 // Create a connection pool
 const pool = createPool({
-   // host: process.env.MYSQL_ADDON_HOST,
-   // user: process.env.MYSQL_ADDON_USER,
-   // password: process.env.MYSQL_ADDON_PASSWORD,
-   // database: process.env.MYSQL_ADDON_DB,
-
+  
     host: 'bo873z9psvcbwj1w0l5p-mysql.services.clever-cloud.com',
     user: 'uxnh0xdp6boktd62',
     password: 'kYh1zZCmRRUtnr87Pbe3',
     database: 'bo873z9psvcbwj1w0l5p',
-    connectionLimit: 5 // Adjust the connection limit as per your requirements
+    connectionLimit: 5 
 });
 
 // test connection
